2b23c8cd86f0c7805d740cb89f64238ae50fd15f
StudioFlows <d@studioflows.co>
2026-07-10 12:32:03 -0400

    Fix homepage live demo route

--- app/page.js
+++ app/page.js
@@ -22,7 +22,7 @@ import {
 } from "@/components/home/InitiationHomeSections";
 
 const C = INITIATION_HOMEPAGE_CONTENT;
-const LIVE_DEMO_URL = "https://os.studioflows.co/demo/access";
+const LIVE_DEMO_URL = "https://os.studioflows.co/demo/real-estate-media";
 const LOGIN_URL = "https://os.studioflows.co/login";
 
 const HOME_HERO = {

d8d52bdd9ef32cb61aac4f8f1cc614a83e5b5f3b
StudioFlows <d@studioflows.co>
2026-07-23 03:05:52 -0400

    Add marketing analytics tags

--- app/layout.js
+++ app/layout.js
@@ -30,6 +30,32 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-BLDEC2BG1R"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-BLDEC2BG1R');
+`,
+          }}
+        />
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+  (function(c,l,a,r,i,t,y){
+      c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+      t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+      y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+  })(window, document, "clarity", "script", "xqtuj2cs6o");
+`,
+          }}
+        />
+      </head>
       <body>{children}</body>
     </html>
   );

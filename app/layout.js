fbc0d800b5c79c004d15f20ba1490ca73a90b6c3
studioflows <d@studioflows.co>
2026-08-10 23:47:26 -0400

    Remove duplicate Clarity loader

--- app/layout.js
+++ app/layout.js
@@ -42,18 +42,6 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 `,
           }}
         />
-        <script
-          type="text/javascript"
-          dangerouslySetInnerHTML={{
-            __html: `
-  (function(c,l,a,r,i,t,y){
-      c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-      t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-      y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-  })(window, document, "clarity", "script", "xqtuj2cs6o");
-`,
-          }}
-        />
       </head>
       <body>
         <noscript>
